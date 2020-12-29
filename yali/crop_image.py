f7834c53b817fa07826431d209d939f0af0eeaf7
yali <yali.ekstein.42@gmail.com>
2020-12-29 16:36:42 +0200

    crop image 2

--- yali/crop_image.py
+++ yali/crop_image.py
@@ -6,21 +6,21 @@ class CropImage:
 
     WINDOW_NAME = f"{ARAZY}"
     MASK = 'mask'
+    R = 3
+    mask = np.zeros((480, 640), np.uint8)
 
     def __init__(self):
-        self.x = None
-        self.y = None
-        self.mask = np.zeros((480, 640), np.uint8)
-        self.cropping = False
+        self.frame = None
 
     def run(self):
         cam = Camera(ARAZY)
         cv2.namedWindow(self.WINDOW_NAME)
         cv2.namedWindow(self.MASK)
-        cv2.setMouseCallback(self.WINDOW_NAME, self.click_and_crop)
+        cv2.setMouseCallback(self.WINDOW_NAME, self.click)
 
         while True:
-            self.frame = cam.get_frame()
+            f = cv2.imread(r'Screenshot 2020-12-29 145155.png')
+            self.frame = f[-480:, :640]  #cam.get_frame()
 
             # Display the resulting frame
             cv2.imshow(self.WINDOW_NAME, self.frame)
@@ -33,41 +33,18 @@ class CropImage:
         cv2.destroyAllWindows()
         cam.end()
 
-    def get_mouse_location(self, event, x, y, flags, param):
-        self.x, self.y = x, y
-
-    def click_and_crop(self, event, x, y, flags, param):
-        # grab references to the global variables
-        # if the left mouse button was clicked, record the starting
-        # (x, y) coordinates and indicate that cropping is being
-        # performed
-        if event == cv2.EVENT_LBUTTONDOWN:
-            self.x, self.y = x, y
-            self.cropping = True
+    def click(self, event, x, y, *args):
         # check to see if the left mouse button was released
-        elif event == cv2.EVENT_LBUTTONUP:
-            # record the ending (x, y) coordinates and indicate that
-            # the cropping operation is finished
-            self.crop(self.x, x, self.y, y)
-            self.cropping = False
+        if event == cv2.EVENT_LBUTTONUP:
+            self.crop(x, y, True)
 
-        elif event == cv2.EVENT_RBUTTONDOWN:
-            self.x, self.y = x, y
-            self.cropping = True
         # check to see if the left mouse button was released
         elif event == cv2.EVENT_RBUTTONUP:
-            # record the ending (x, y) coordinates and indicate that
-            # the cropping operation is finished
-            self.crop(self.x, x, self.y, y)
-            self.cropping = False
+            self.crop(x, y, False)
 
-    def crop(self, x1, x2, y1, y2):
-        start_x = min(x1, x2)
-        end_x = max(x1, x2)
-        start_y = min(y1, y2)
-        end_y = max(y1, y2)
-
-        self.mask[start_y:end_y, start_x:end_x] = 255 if self.cropping else 0
+    def crop(self, x, y, crop):
+        r = self.R if crop else int(1.5*self.R)
+        self.mask[y-r:y+r, x-r:x+r] = 255 if crop else 0
 
 
 if __name__ == '__main__':
